b5092b667a09b24ae5eddf99983304543a3a1bcf
Eragonbro <chafouri.moncif@gmail.com>
2024-12-22 15:37:49 +0100

    add CSS and JS structure

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// variable declaration
+
+// step 1 data
+
+// step 2 data
+
+// step 3 data recap
+
+// multistep navigation
+
+    // step navigation
+
+        //validate step 1
+
+        //move to step 2
+
+        //move back to step 1
+
+        //move to step 3
+
+        //populate recap
+
+        //move back to step 2
+
+        //
